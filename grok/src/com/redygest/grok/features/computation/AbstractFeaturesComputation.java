6d93abbb6df25d62804d47b412376ef257104bb1
Sudheer <sudheer@sudheer-desktop.(none)>
2011-05-09 01:32:48 -0700

    added merge function for addGlobalFeatures. use overrite=false only for real numbers

--- grok/src/com/redygest/grok/features/computation/AbstractFeaturesComputation.java
+++ grok/src/com/redygest/grok/features/computation/AbstractFeaturesComputation.java
@@ -6,6 +6,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.redygest.grok.features.datatype.AttributeType;
+import com.redygest.grok.features.datatype.Attributes;
+import com.redygest.grok.features.datatype.DataVariable;
 import com.redygest.grok.features.datatype.FeatureVector;
 import com.redygest.grok.features.datatype.Variable;
 
@@ -42,11 +45,43 @@ public abstract class AbstractFeaturesComputation {
 						&& featureVector.getVariable(variable) != null) {
 					featureVector.addVariable(variable);
 				} else {
-					// TODO merge
+					Variable existingVariable = featureVector.getVariable(variable);
+					featureVector.addVariable(merge(variable, existingVariable));
 				}
 			}
 		} else {
 			this.featureVectors.put(GLOBAL_IDENTIFIER, fv);
 		}
+	}
+	
+	private Variable merge(Variable newVariable, Variable existingVariable) {
+		Attributes newAttrs = newVariable.getVariableAttributes();
+		Attributes existingAttrs = existingVariable.getVariableAttributes();
+		Attributes rAttrs = new Attributes();
+		rAttrs.putAll(existingAttrs);
+		for(AttributeType value : newAttrs.values()) {
+				List<String> newAttributeNames = newAttrs.getAttributeNames(value);
+				if(newAttributeNames != null && newAttributeNames.size() > 1) {
+					throw new RuntimeException("cannot update a multi valued attribute type in newAttributeNames");
+				}
+				List<String> existingAttributeNames = existingAttrs.getAttributeNames(value);
+				if(existingAttributeNames != null && existingAttributeNames.size() > 1) {
+					throw new RuntimeException("cannot update a multi valued attribute type in existingAttributeNames");
+				}
+				
+				if(newAttributeNames != null) {
+					String newAttributeName = newAttributeNames.get(0);
+					if(existingAttributeNames != null) {
+						String existingAttributeName = existingAttributeNames.get(0);
+						Double rAttributeName = Double.valueOf(newAttributeName) + Double.valueOf(existingAttributeName);
+						rAttrs.put(Double.toString(rAttributeName), value);
+					} else {
+						rAttrs.put(newAttributeName, value);
+					}
+				}				
+		}
+		DataVariable rVariable = new DataVariable(existingVariable.getVariableName(), existingVariable.getRecordIdentifier());
+		rVariable.addAttributes(rAttrs);
+		return rVariable;
 	}
 }
