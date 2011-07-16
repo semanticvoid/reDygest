1c98df136e601c985298e6164634d9012ff01b0f
semanticvoid <anand@semanticvoid.com>
2011-07-16 10:29:50 -0700

    fixing earlier commit for senna

--- grok/src/com/redygest/grok/srl/Senna.java
+++ grok/src/com/redygest/grok/srl/Senna.java
@@ -8,21 +8,41 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import com.redygest.commons.cache.CacheFactory;
+import com.redygest.commons.cache.ICache;
+import com.redygest.commons.cache.CacheFactory.CacheType;
+
 public class Senna {
-	
+
 	String[] lineArr;
+	File dir;
+	ICache cache;
 
-	File sennaInstallationDir = new File("/Users/tejaswi/Documents/StanfordCourses/SRL/senna-v2.0");
-
-	/*
-	 * @ 
+	/**
+	 * Constructor
+	 * 
+	 * @param path
+	 *            - the path to the senna directory
 	 */
+	public Senna(String path) {
+		dir = new File(path);
+		init();
+	}
+
+	public Senna() {
+		init();
+	}
+
+	private void init() {
+		cache = CacheFactory.getInstance().produceCache(CacheType.MEMORY,
+				"senna");
+	}
+
 	public String getSennaOutput(String line) {
 		try {
-			String cmd = "echo " + line + " | " + sennaInstallationDir
-					+ "/senna ";
+			String cmd = "echo " + line + " | " + dir + "/senna ";
 			ProcessBuilder pb = new ProcessBuilder("bash", "-c", cmd);
-			pb.directory(sennaInstallationDir);
+			pb.directory(dir);
 			Process shell = pb.start();
 			InputStream shellIn = shell.getInputStream();
 			int shellExitStatus = shell.waitFor();
@@ -37,148 +57,211 @@ public class Senna {
 		}
 		return null;
 	}
-	
-	
 
-	public HashMap<String, SennaVerb>  parseSennaLines(String allText, String sentence){	
+	public HashMap<String, SennaVerb> parseSennaLines(String allText,
+			String sentence) {
 		lineArr = allText.split("\n");
 		HashMap<String, SennaVerb> verbsToArgs = new HashMap<String, SennaVerb>();
 		ArrayList<SennaVerb> verbs = new ArrayList<SennaVerb>();
 		int verbCount = 0;
-		for(int i=0; i < lineArr.length; i++) {
-			//String[] line = lineArr[i].trim().split("\t");
+		for (int i = 0; i < lineArr.length; i++) {
+			// String[] line = lineArr[i].trim().split("\t");
 			String line = lineArr[i].trim();
-			if(line.length()==0)
+			if (line.length() == 0)
 				continue;
 			lineArr[i] = line;
 			Pattern p = Pattern.compile("VB[A-Z]?\t");
-			//System.out.println(line);
-			if(( !line.split("\\s+")[4].trim().equalsIgnoreCase("-"))  ){
-				//Verb v = getVerbArguments(++verbCount, line.split("\\s+")[0].trim(), sentence);
-				SennaVerb v = getVerbArgumentNPs(++verbCount, line.split("\\s+")[0].trim(), sentence);
+			// System.out.println(line);
+			if ((!line.split("\\s+")[4].trim().equalsIgnoreCase("-"))) {
+				// Verb v = getVerbArguments(++verbCount,
+				// line.split("\\s+")[0].trim(), sentence);
+				SennaVerb v = getVerbArgumentNPs(++verbCount, line
+						.split("\\s+")[0].trim(), sentence);
 				verbs.add(v);
-				verbsToArgs.put(v.text, v);
+				verbsToArgs.put(v.getText(), v);
 			}
 		}
-		return verbsToArgs; 
+		return verbsToArgs;
 	}
-	
-	public SennaVerb getVerbArgumentNPs(int index, String verb, String sentence){
-		SennaVerb v = new SennaVerb();
-		v.text = verb;
+
+	public SennaVerb getVerbArgumentNPs(int index, String verb, String sentence) {
+		SennaVerb v = new SennaVerb(verb);
 		HashMap<String, List<String>> argumentToText = new HashMap<String, List<String>>();
 		index = index + 4;
-		
-		for(int i=0; i < lineArr.length; i++){
-			try{
-			String[] lineTokens =  lineArr[i].trim().split("\\s+");			
-			String token = lineTokens[0].trim();
-			String pos = lineTokens[1].trim();
-			String value = lineTokens[index].trim();
-			
-			if(value.equals("O")){
-				continue;
-			} else if(value.startsWith("S-") && !value.contains("S-V") && pos.contains("NN")) {
-				String arg = value.split("S-")[1];
-				ArrayList<String> arr_token = new ArrayList<String>();
-				arr_token.add(token);
-				argumentToText.put(arg, arr_token);
-//			} else if(value.startsWith("B-") && Character.isDigit(value.charAt(value.length()-1))) {
-			} else if(value.startsWith("B-")) {
-				String arg = value.split("B-")[1];
-				StringBuilder text = new StringBuilder();
-				boolean flag = false;
-				if(pos.contains("NN")){
-				  text.append(token);
-				  flag = true;
+
+		for (int i = 0; i < lineArr.length; i++) {
+			try {
+				String[] lineTokens = lineArr[i].trim().split("\\s+");
+				String token = lineTokens[0].trim();
+				String pos = lineTokens[1].trim();
+				String value = lineTokens[index].trim();
+
+				if (value.equals("O")) {
+					continue;
+				} else if (value.startsWith("S-") && !value.contains("S-V")
+						&& pos.contains("NN")) {
+					String arg = value.split("S-")[1];
+					ArrayList<String> arr_token = new ArrayList<String>();
+					arr_token.add(token);
+					argumentToText.put(arg, arr_token);
+					// } else if(value.startsWith("B-") &&
+					// Character.isDigit(value.charAt(value.length()-1))) {
+				} else if (value.startsWith("B-")) {
+					String arg = value.split("B-")[1];
+					StringBuilder text = new StringBuilder();
+					boolean flag = false;
+					if (pos.contains("NN")) {
+						text.append(token);
+						flag = true;
+					}
+					while (!value.startsWith("E-")) {
+						i++;
+						lineTokens = lineArr[i].trim().split("\\s+");
+						token = lineTokens[0].trim();
+						pos = lineTokens[1].trim();
+						value = lineTokens[index];
+						if (pos.contains("NN")) {
+							text.append(" " + token);
+							flag = true;
+						} else if (flag == true) {
+							if (argumentToText.containsKey(arg))
+								argumentToText.get(arg).add(
+										text.toString().trim());
+							else {
+								ArrayList<String> nps = new ArrayList<String>();
+								nps.add(text.toString().trim());
+								argumentToText.put(arg, nps);
+							}
+							text = new StringBuilder();
+							flag = false;
+						}
+					}
+
+					if (value.startsWith("E-") && flag == true) {
+						if (argumentToText.containsKey(arg))
+							argumentToText.get(arg).add(text.toString().trim());
+						else {
+							ArrayList<String> nps = new ArrayList<String>();
+							nps.add(text.toString().trim());
+							argumentToText.put(arg, nps);
+						}
+						flag = false;
+					}
+
+					if (argumentToText.containsKey(arg))
+						arg = arg + "-1";
+
 				}
-				while(!value.startsWith("E-")){
-					 i++;
-					 lineTokens =  lineArr[i].trim().split("\\s+");						
-					 token = lineTokens[0].trim();
-					 pos = lineTokens[1].trim();
-					 value = lineTokens[index];
-					 if(pos.contains("NN")){
-						 text.append(" "+token);
-						 flag = true;
-					 }
-					 else if(flag==true){
-						 if(argumentToText.containsKey(arg))
-							 argumentToText.get(arg).add(text.toString().trim());
-						 else{
-							 ArrayList<String> nps = new ArrayList<String>();
-							 nps.add(text.toString().trim());
-							 argumentToText.put(arg, nps);
-						 }
-						 text = new StringBuilder();
-						 flag = false;
-					 }
-				}
-				
-				if(value.startsWith("E-") && flag==true){					
-					 if(argumentToText.containsKey(arg))
-						 argumentToText.get(arg).add(text.toString().trim());
-					 else{
-						 ArrayList<String> nps = new ArrayList<String>();
-						 nps.add(text.toString().trim());
-						 argumentToText.put(arg, nps);
-					 }
-					 flag = false;
-				}
-				
-				if(argumentToText.containsKey(arg))
-					arg = arg + "-1";
-				
-			}
-			}catch(Exception e){
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
-		v.argumentToNPs = argumentToText;
+
+		v.setArgumentToNPs(argumentToText);
 		return v;
 
 	}
-	
-	//get the arguments of a SennaVerb
-	public SennaVerb getSennaVerbArguments(int index, String verb, String sentence){
-		
+
+	// get the arguments of a SennaVerb
+	public SennaVerb getSennaVerbArguments(int index, String verb,
+			String sentence) {
+
 		List<String> words = Arrays.asList(sentence.trim().split("\\s+"));
-		//System.out.println(sentence);
-		SennaVerb v = new SennaVerb();
-		v.text = verb;
+		// System.out.println(sentence);
+		SennaVerb v = new SennaVerb(verb);
 		HashMap<String, String> argumentToText = new HashMap<String, String>();
 		index = index + 4;
-		//System.out.println("index "+index);
-		for(int i=0; i < lineArr.length; i++){
-			//String[] arr = lineArr[i].split("\\s+");
-			String value =  lineArr[i].trim().split("\\s+")[index].trim();
-			if(value.equals("O")){
+		// System.out.println("index "+index);
+		for (int i = 0; i < lineArr.length; i++) {
+			// String[] arr = lineArr[i].split("\\s+");
+			String value = lineArr[i].trim().split("\\s+")[index].trim();
+			if (value.equals("O")) {
 				continue;
-			} else if(value.startsWith("S-") && !value.contains("S-V")){
+			} else if (value.startsWith("S-") && !value.contains("S-V")) {
 				String arg = value.split("S-")[1];
-				//String text = lineArr[i].trim().split("\\s+")[0].trim();
+				// String text = lineArr[i].trim().split("\\s+")[0].trim();
 				String text = words.get(i).trim();
 				argumentToText.put(arg, text);
-			}
-			else if(value.startsWith("B-") && Character.isDigit(value.charAt(value.length()))){
+			} else if (value.startsWith("B-")
+					&& Character.isDigit(value.charAt(value.length()))) {
 				System.out.println(value);
 				String arg = value.split("B-")[1];
-				//String text = lineArr[i].trim().split("\\s+")[0].trim();
+				// String text = lineArr[i].trim().split("\\s+")[0].trim();
 				String text = words.get(i).trim();
-				while(!value.startsWith("E-")){
-					 i++;
-					 value =  lineArr[i].trim().split("\\s+")[index].trim();
-					 //text += (" "+lineArr[i].trim().split("\\s+")[0].trim());
-					 text += (" "+words.get(i).trim());
+				while (!value.startsWith("E-")) {
+					i++;
+					value = lineArr[i].trim().split("\\s+")[index].trim();
+					// text += (" "+lineArr[i].trim().split("\\s+")[0].trim());
+					text += (" " + words.get(i).trim());
 				}
-				if(argumentToText.containsKey(arg))
+				if (argumentToText.containsKey(arg))
 					arg = arg + "-1";
 				argumentToText.put(arg, text);
 			}
 		}
-		v.argumentToText = argumentToText;
+		v.setArgumentToText(argumentToText);
 		return v;
 	}
 
+	/**
+	 * 
+	 * @param sennaOutput
+	 *            - senna output for a sentence
+	 * @return
+	 */
+	public ArrayList<String> getNounPhrases(String sennaOutput) {
+		// System.out.println(sennaOutput);
+		ArrayList<String> nounPhrases = new ArrayList<String>();
+		String[] lineArr = sennaOutput.split("\n");
+
+		ArrayList<String> words = new ArrayList<String>();
+		ArrayList<String> posTags = new ArrayList<String>();
+		ArrayList<String> chunkerTokens = new ArrayList<String>();
+		try {
+			for (String line : lineArr) {
+				String[] tokens = line.trim().split("[ \t]+");
+				if (tokens.length < 3)
+					continue;
+				words.add(tokens[0].trim());
+				posTags.add(tokens[1].trim());
+				chunkerTokens.add(tokens[2].trim());
+			}
+
+			boolean flag = false;
+			StringBuffer npBuf = new StringBuffer();
+			for (int i = 0; i < posTags.size(); i++) {
+				if (posTags.get(i).startsWith("NN")
+						|| posTags.get(i).startsWith("JJ")) {
+					flag = true;
+					npBuf.append(words.get(i) + " ");
+				} else if (flag == true) {
+					flag = false;
+					nounPhrases.add(npBuf.toString().trim().toLowerCase());
+					npBuf = new StringBuffer();
+				}
+			}
+			if (flag == true)
+				nounPhrases.add(npBuf.toString().trim().toLowerCase());
+
+			// for (int i = 0; i < chunkerTokens.size(); i++) {
+			// if (chunkerTokens.get(i).equalsIgnoreCase("s-np")) {
+			// nounPhrases.add(words.get(i).toLowerCase());
+			// } else if (chunkerTokens.get(i).equalsIgnoreCase("b-np")) {
+			// String np = "";
+			// while (!chunkerTokens.get(i).equalsIgnoreCase("e-np")) {
+			// //System.out.println(chunkerTokens.get(i));
+			// if (posTags.get(i).contains("NN")
+			// || posTags.get(i).contains("JJ"))
+			// np += (words.get(i) + " ");
+			// i++;
+			// }
+			// np += (words.get(i) + " ");
+			// nounPhrases.add(np.trim().toLowerCase());
+			// }
+			// }
+		} catch (Exception e) {
+			// e.printStackTrace();
+		}
+		return nounPhrases;
+	}
 }
--- grok/src/com/redygest/grok/srl/SennaVerb.java
+++ grok/src/com/redygest/grok/srl/SennaVerb.java
@@ -4,9 +4,59 @@ import java.util.HashMap;
 import java.util.List;
 
 public class SennaVerb {
-	
-	public String text;
-	public HashMap<String, String> argumentToText = new HashMap<String, String>();
-	public HashMap<String, List<String>> argumentToNPs = new HashMap<String, List<String>>();
-	
+
+	private String text;
+	private HashMap<String, String> argumentToText = new HashMap<String, String>();
+	private HashMap<String, List<String>> argumentToNPs = new HashMap<String, List<String>>();
+
+	/**
+	 * Constructor
+	 */
+	public SennaVerb() {
+	}
+
+	/**
+	 * Constructor
+	 * 
+	 * @param text
+	 */
+	public SennaVerb(String text) {
+		this.text = text;
+	}
+
+	/**
+	 * get text
+	 * 
+	 * @return
+	 */
+	public String getText() {
+		return this.text;
+	}
+
+	/**
+	 * set argument to text
+	 * 
+	 * @param map
+	 */
+	public void setArgumentToText(HashMap<String, String> map) {
+		this.argumentToText = map;
+	}
+
+	/**
+	 * set argument to NPs
+	 * 
+	 * @param map
+	 */
+	public void setArgumentToNPs(HashMap<String, List<String>> map) {
+		this.argumentToNPs = map;
+	}
+
+	public HashMap<String, String> getArgumentToText() {
+		return argumentToText;
+	}
+
+	public HashMap<String, List<String>> getArgumentToNPs() {
+		return argumentToNPs;
+	}
+
 }
