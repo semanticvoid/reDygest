c0b40d86337f67a9679ad2fdf9144dce08e75fc9
semanticvoid <anand@semanticvoid.com>
2011-07-18 08:49:57 -0700

    #7 ignore

--- grok/src/com/redygest/grok/knowledge/Curator.java
+++ grok/src/com/redygest/grok/knowledge/Curator.java
@@ -1,9 +1,14 @@
 package com.redygest.grok.knowledge;
 
 import java.util.List;
+import java.util.Set;
 
 import com.redygest.grok.knowledge.graph.IRepresentation;
+import com.redygest.grok.knowledge.graph.Node;
+import com.redygest.grok.knowledge.graph.Relation;
 import com.redygest.grok.knowledge.graph.RepresentationFactory;
+import com.redygest.grok.knowledge.graph.Node.NodeType;
+import com.redygest.grok.knowledge.graph.Relation.Relationship;
 import com.redygest.grok.knowledge.graph.RepresentationFactory.RepresentationType;
 
 /**
@@ -28,6 +33,29 @@ public class Curator {
 	 * @return true on sucess false otherwise
 	 */
 	public boolean addEvents(List<Event> events) {
+		if(events != null) {
+			// add node for sentence
+			Node sentence = new Node(NodeType.SENTENCE);
+			kmodel.addNode(sentence);
+			for(Event e : events) {
+				// add node for event
+				Node event = new Node(NodeType.EVENT);
+				kmodel.addNode(event);
+				
+				// attach event to sentence
+				Relation rel = new Relation(Relationship.EVENT, sentence, event);
+				kmodel.addRelation(rel);
+				
+				// iterate over arguments and create/add entities
+				Set<String> keys = e.getArgumentToText().keySet();
+				if(keys != null) {
+					for(String key : keys) {
+						
+					}
+				}
+			}
+		}
+		
 		return false;
 	}
 	
