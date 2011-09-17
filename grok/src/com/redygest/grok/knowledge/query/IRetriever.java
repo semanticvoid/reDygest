ebe13eb9837b06b837c2a7430443c115ada40571
semanticvoid <anand@semanticvoid.com>
2011-09-17 11:03:47 -0700

    rename query transformer to retriever

---
+++ grok/src/com/redygest/grok/knowledge/query/IRetriever.java
@@ -0,0 +1,12 @@
+package com.redygest.grok.knowledge.query;
+
+import java.util.Collection;
+
+import com.redygest.grok.knowledge.query.datatype.Entity;
+import com.redygest.grok.knowledge.query.datatype.Result;
+
+public interface IRetriever {
+	Collection<Result> query(String freeText);
+	
+	Collection<Result> query(Entity n1, Entity r, Entity n2);
+}
---
+++ grok/src/com/redygest/grok/knowledge/query/Neo4jRetriever.java
@@ -0,0 +1,22 @@
+package com.redygest.grok.knowledge.query;
+
+import java.util.Collection;
+
+import com.redygest.grok.knowledge.query.datatype.Entity;
+import com.redygest.grok.knowledge.query.datatype.Result;
+
+public class Neo4jRetriever implements IRetriever {
+
+	@Override
+	public Collection<Result> query(String freeText) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Result> query(Entity n1, Entity r, Entity n2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
