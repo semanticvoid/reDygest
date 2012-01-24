eeed8ea75ffa6c9797ae660dc01b6f1d52538010
semanticvoid <anand@semanticvoid.com>
2012-01-24 01:02:11 -0800

    SimpleDB singleton wrapper

---
+++ grok/src/com/redygest/commons/db/nosql/SimpleDB.java
@@ -0,0 +1,36 @@
+package com.redygest.commons.db.nosql;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.simpledb.AmazonSimpleDBClient;
+import com.redygest.commons.config.ConfigReader;
+
+/**
+ * Simple DB singleton
+ *
+ */
+public class SimpleDB extends AmazonSimpleDBClient {
+
+	private static SimpleDB instance = null;
+	
+	private SimpleDB(AWSCredentials creds) {
+		super(creds);
+	}
+	
+	public static SimpleDB getDB() {
+		if(instance == null) {
+			ConfigReader config = ConfigReader.getInstance();
+			try {
+				instance = new SimpleDB(new PropertiesCredentials(new File(config.getAWSCredentialsPath())));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return instance;
+	}
+}
