be16eb08b9222400b3d5180ed6973ed8a2193295
semanticvoid <anand@semanticvoid.com>
2012-03-12 17:39:20 -0700

    return if score > threshold

--- grok/src/main/java/com/redygest/redundancy/similarity/BaseLineDataSimilarity.java
+++ grok/src/main/java/com/redygest/redundancy/similarity/BaseLineDataSimilarity.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import com.redygest.commons.data.Data;
 import com.redygest.redundancy.similarity.score.ISimilarityScore;
-import com.redygest.redundancy.similarity.score.SimScoreFactory;
-import com.redygest.redundancy.similarity.score.SimScoreFactory.Score;
 
 public class BaseLineDataSimilarity implements IDataSimilarity {
 	List<ISimilarityScore> scoringFunctions = null;
@@ -16,19 +14,18 @@ public class BaseLineDataSimilarity implements IDataSimilarity {
 
 	public double similarity(Data d1, Data d2) {
 		double highestScore = 0.0;
+
 		for (ISimilarityScore iss : scoringFunctions) {
 			try {
-				double cur_score = iss.score(d1, d2);
-				if (cur_score > highestScore) {
-					highestScore = cur_score;
+				double curScore = iss.score(d1, d2);
+				if (curScore >= 0.5) {
+					return 1.0;
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		if (highestScore >= 0.5) {
-			return 1.0;
-		}
+
 		return 0;
 	}
 
