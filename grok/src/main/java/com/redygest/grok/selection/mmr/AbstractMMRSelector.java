43350d456d528cf0f4827e51263084bf225932cf
semanticvoid <anand@semanticvoid.com>
2012-04-08 11:40:45 -0700

    implementation of mmr logic in abstract class

--- grok/src/main/java/com/redygest/grok/selection/mmr/AbstractMMRSelector.java
+++ grok/src/main/java/com/redygest/grok/selection/mmr/AbstractMMRSelector.java
@@ -1,8 +1,10 @@
 package com.redygest.grok.selection.mmr;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.redygest.commons.data.Data;
+import com.redygest.commons.data.DataType;
 import com.redygest.commons.data.Query;
 import com.redygest.grok.selection.ISelector;
 import com.redygest.score.IScore;
@@ -63,8 +65,47 @@ public abstract class AbstractMMRSelector implements ISelector {
 	 * @return
 	 */
 	public List<Data> select(int size, Query query) {
-		// TODO basic MMR logic
-		return null;
+		List<Integer> selectedIndices = new ArrayList<Integer>();
+		List<Data> selectedData = new ArrayList<Data>();
+
+		// extract only n=size number of items
+		for (int i = 0; i < size; i++) {
+			// always add index '0' as the first element
+			if (i == 0) {
+				selectedIndices.add(i);
+			} else {
+				// bookkeeping for index with max mmr score
+				double maxMMRScore = 0;
+				int maxMMRScoreIndex = -1;
+
+				// iterate through the entire ranked list
+				for (int j = 0; j < rankedData.size(); j++) {
+					// ignore rankedData if its already been selected
+					if (selectedIndices.contains(j)) {
+						continue;
+					} else {
+						// TODO increment lambda
+						double mmrScore = computeMMR(rankedData.get(j), query,
+								selectedIndices, initialLambda);
+						// save score and index if max (yet)
+						if (mmrScore > maxMMRScore) {
+							maxMMRScore = mmrScore;
+							maxMMRScoreIndex = j;
+						}
+					}
+				}
+
+				// add max score index as selected
+				selectedIndices.add(maxMMRScoreIndex);
+			}
+		}
+
+		// form selected data list
+		for (Integer i : selectedIndices) {
+			selectedData.add(rankedData.get(i));
+		}
+
+		return selectedData;
 	}
 
 	/**
@@ -77,6 +118,41 @@ public abstract class AbstractMMRSelector implements ISelector {
 	public List<Data> select(int size) {
 		// not supported
 		return null;
+	}
+
+	/**
+	 * Function to compute the MMR score
+	 * 
+	 * @param d
+	 * @param q
+	 * @param selectedIndices
+	 * @param lambda
+	 * @return the mmr score
+	 */
+	protected double computeMMR(Data d, Query q, List<Integer> selectedIndices,
+			double lambda) {
+		double relevanceScore = 0;
+
+		// get relevance score
+		if (d != null && d.getValue(DataType.SCORE) != null) {
+			relevanceScore = Double.valueOf(d.getValue(DataType.SCORE));
+		}
+
+		// get max redundancy score by comparing
+		// against already selected data
+		double maxRedundancyScore = 0;
+		for (Integer i : selectedIndices) {
+			Data selectedData = rankedData.get(i);
+			double score = this.redundancyScore.score(selectedData, d);
+			if (score > maxRedundancyScore) {
+				maxRedundancyScore = score;
+			}
+		}
+
+		// compute mmr score
+		double mmrScore = (lambda * relevanceScore)
+				- ((1 - lambda) * maxRedundancyScore);
+		return mmrScore;
 	}
 
 	/**
