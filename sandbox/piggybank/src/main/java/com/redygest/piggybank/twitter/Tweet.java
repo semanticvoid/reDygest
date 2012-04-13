0262ef2f7681af007cfd93e4ed15465a44dc1c4d
semanticvoid <anand@semanticvoid.com>
2012-04-13 14:25:51 -0700

    piggybank

---
+++ sandbox/piggybank/src/main/java/com/redygest/piggybank/twitter/Tweet.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.redygest.piggybank.twitter;
+
+import java.io.IOException;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+
+/**
+ * Piggybank Tweet Class
+ * 
+ */
+public class Tweet extends EvalFunc<Tuple> {
+
+	@Override
+	public Tuple exec(Tuple arg0) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
