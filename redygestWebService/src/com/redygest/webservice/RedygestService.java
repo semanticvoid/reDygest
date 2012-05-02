1595f45b4b1586c46c3f41bd06f80590dceafc0d
semanticvoid <anand@semanticvoid.com>
2012-05-02 00:24:22 -0700

    removed old webservice code + python

--- redygestWebService/src/com/redygest/webservice/RedygestService.java
+++
@@ -1,7 +0,0 @@
-package com.redygest.webservice;
-
-import com.redygest.webservice.model.Results;
-
-public interface RedygestService {
-	public Results getQueryResults(String hw1, String r, String hw2);
-}
