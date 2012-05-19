e0c73494921a33510a0072f37ad6b4c749a8222a
semanticvoid <anand@semanticvoid.com>
2012-05-18 17:34:51 -0700

    fixed confu

--- sandbox/facop/script/confu.pl
+++ sandbox/facop/script/confu.pl
@@ -4,13 +4,14 @@ use Data::Dumper;
 
 my $labeled = shift @ARGV;
 my $predicted = shift @ARGV;
+my $threshold = shift @ARGV;
 
 my %labels = ();
 
 open LABEL, $labeled or die $!;
 while(<LABEL>) {
 	chomp;
-	my ($l, $w, $id) = split("[ ]+", $_);
+	my ($l, $w, $id) = split("[| ]+", $_);
 	$labels{$id} = $l;
 }
 
@@ -18,14 +19,22 @@ my ($fp, $fn, $tp, $tn) = (0,0,0,0);
 
 open PREDICT, $predicted or die $!;
 while(<PREDICT>) {
-	my ($l, $w, $id) = split("[ ]+", $_);
+	chomp;
+	my ($l, $id) = split("[ ]+", $_);
+
+	if($l > $threshold) {
+		$l = 1;
+	} else {
+		$l = 0;
+	}
+
 	if($l eq '1' and $labels{$id} eq $l) {
 		$tp++;
-	} else if($l eq '1' and $labels{$id} ne $l) {
+	} elsif($l eq '1' and $labels{$id} ne $l) {
 		$fp++;
-	} else if($l eq '0' and $labels{$id} eq $l) {
+	} elsif($l eq '0' and $labels{$id} eq $l) {
 		$tn++;
-	} else if($l eq '0' and $labels{$id} ne $l) {
+	} elsif($l eq '0' and $labels{$id} ne $l) {
 		$fn++;
 	} 
 }
