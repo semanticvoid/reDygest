407513b41cff3a0dff0ae30ac8b12bd2a011bb4d
sudheer624 <sudheer.624@gmail.com>
2012-06-04 00:23:30 -0700

    migrating ui frm php to spring based web application

---
+++ webui/src/com/redygest/webui/controller/StoryController.java
@@ -0,0 +1,24 @@
+package com.redygest.webui.controller;
+
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class StoryController implements Controller {
+    protected final Log logger = LogFactory.getLog(getClass());
+
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        logger.info("Returning story view");
+
+        return new ModelAndView("story.jsp");
+    }
+}
